61d8dbf6d60fd5f10764eeb0531f60e09dbefbb0
Md Asaduzaman <asadasad9282@gmail.com>
2022-03-18 17:43:47 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <article>
+
+        <h2 style={{ color: 'red', textAlign: 'center', marginTop: '20px', background: 'blue' }} >This is article</h2>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab consequatur quia in qui nobis libero, adipisci dolore quos voluptas animi beatae impedit. Ipsa vero in corrupti, harum, cupiditate aliquid officiis voluptates, rerum recusandae cumque dolorum? Asperiores temporibus, corrupti explicabo, blanditiis molestias laborum labore distinctio possimus facilis fugit nemo dolorum repellendus.</article>
+    </div>
+  );
+};
+
+export default App;
+
+
+
+
